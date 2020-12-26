5f6b7978e71968378b08da13e9303271e69c02ed
andim0n <adpetrov11@gmail.com>
2020-12-26 23:44:07 +0100

    2 tasks gitignore file

---
+++ src/basics_of_software_code_development/linear_programs/TaskOne.java
@@ -0,0 +1,12 @@
+package basics_of_software_code_development.linear_programs;
+
+public class TaskOne {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        System.out.println(" z = ((" + a + " - 3) * " + b + " / 2) + " + c);
+        int z = ((a - 3) * b / 2) + c;
+        System.out.print(" z = " + z);
+    }
+}
---
+++ src/basics_of_software_code_development/linear_programs/TaskTwo.java
@@ -0,0 +1,13 @@
+package basics_of_software_code_development.linear_programs;
+
+import java.lang.Math;
+
+class TaskTwo {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double c = 3;
+        double x = ((b + Math.sqrt(Math.pow(b, 2) + 4 * a * c)) / (2 * a)) - (Math.pow(a, 3) * c + Math.pow(b, -2));
+        System.out.print(x);
+    }
+}
