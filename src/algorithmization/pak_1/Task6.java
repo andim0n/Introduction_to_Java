6144905d9f1251d1bdd3619500e18ba0f35addea
andimon-pc <adpetrov11@gmail.com>
2021-01-12 12:48:34 +0300

    12/1

--- src/algorithmization/pak_1/Task6.java
+++ src/algorithmization/pak_1/Task6.java
@@ -4,14 +4,19 @@ import algorithmization.utils.MyUtil;
 
 public class Task6 {
     public static void main(String[] main) {
-        int n = 10;
+        int n = 30;
+        int sum = 0;
         int[] array = MyUtil.setArray(n);
         for (int i = 0; i < array.length; i++) {
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     break;
                 }
+                if (j == i - 1) {
+                    sum += array[i];
+                }
             }
         }
+        System.out.printf("\n Sum = %d", sum);
     }
 }
--- src/algorithmization/utils/MyUtil.java
+++ src/algorithmization/utils/MyUtil.java
@@ -1,18 +1,18 @@
 package algorithmization.utils;
 
 public class MyUtil {
-    public static int[] setArray(int n){
+    public static int[] setArray(int n) {
         int[] array = new int[n];
-        for(int i = 0; i< array.length; i++){
-            array[i] = (int)(Math.random() * 100 - 30);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10) + 1;
             System.out.printf("%d ", array[i]);
         }
         return array;
     }
 
-    public static void printArray(int[] array){
+    public static void printArray(int[] array) {
         System.out.printf("\nArray:\n");
-        for(int a : array){
+        for (int a : array) {
             System.out.printf("%d ", a);
         }
     }
